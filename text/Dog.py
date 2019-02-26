5d360e67baa250180cfea7e7e6a62f1b0e03895e
waggag <waggag@outlook.com>
2019-02-26 17:38:52 +0800

    类的练习

--- text/Dog.py
+++ text/Dog.py
@@ -1,7 +1,7 @@
 
 class Dog:
-    """一次模拟小狗的简单尝试"""
-
+    """一次模拟小狗的简单尝试,可以生成文档"""
+	# ~ 需要指定self，但不需要给其传实参                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
     def __init__(self, name, age):
         """初始化属性name和age"""
         self.name = name
--- text/making_pizz.py
+++ text/making_pizz.py
@@ -1,4 +1,18 @@
 from pizza import make_pizza as mp
+from collections import OrderedDict 
 # ~ 使用as指定模块的别名
 mp(16, 'pepperoni')
 mp(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# ~ Python的标准库,OrderedDict是有序字典
+favorite_language= OrderedDict()
+
+favorite_language['jen'] = 'python'
+favorite_language['jen1'] = 'c'
+favorite_language['jen2'] = 'java'
+favorite_language['jen3'] = 'ruby'
+
+for name,value in favorite_language.items():
+	print(name.title()+" favorite language "+value.title()+".")
+
+                 
